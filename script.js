877bf3a873abe743776b56d984e241ba09233104
macauguste <mckernauguste@gmail.com>
2021-07-16 17:12:11 -0400

    Basic operatiors/precedence

--- script.js
+++ script.js
@@ -70,5 +70,37 @@ var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
 
+//Multiple operators
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge)
+
+//Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+//Multiple Assignments 
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 x 4 - 6// 32 - 6 //26
+console.log(x, y)
+
+//More Operators
+//x=x *2;
+
+x *= 2;
+
+console.log(x);
+
+x +=10;
+
+console.log(x);
+
+x++;
+
+console.log(x);
+
+x--;
+
+console.log(x);
+
