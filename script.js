212caf11ffc13a9e39bfa3a8f1b17432133b354c
macauguste <mckernauguste@gmail.com>
2021-07-19 13:01:50 -0400

    compound else if

--- script.js
+++ script.js
@@ -157,4 +157,19 @@ if (markBMI > johnBMI) {
 }
 */
 
+//Compound Else IF
+
+/*
+var firstName = 'John ';
+var age = 30;
+
+if (age < 13) {
+    console.log(firstName + 'is a boy');
+} else if (age >= 13 && age < 18) {
+    console.log(firstName + 'is a teenager');
+} else {
+    console.log(firstName + 'is a man');
+}
+*/
+
 
