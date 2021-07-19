78f2d7f03795b6b1ab3286af2b2b9354a5b46b48
macauguste <mckernauguste@gmail.com>
2021-07-19 19:58:52 -0400

    Loops and Iterations

--- script.js
+++ script.js
@@ -461,3 +461,47 @@ if (john.calcBMI() > mark.calcBMI()){
 console.log(john);
 console.log(mark);
 */
+
+///Loops and Iterations
+//for
+/*
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (var i = 0; i < 10; i+= 2) {
+    console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
+
+//while
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++
+}
+
+//continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, "blue"];
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue; 
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break; 
+    console.log(john[i]);
+}
+
+//Looping backwards
+for (var i = john.length - 1; i >= 0 ; i--) {
+    if (typeof john[i] !== 'string') break; 
+    console.log(john[i]);
+}
+*/
