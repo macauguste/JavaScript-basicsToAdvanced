bbf14ebca8f473a2d0691a776c819cab918eb31f
macauguste <mckernauguste@gmail.com>
2021-07-20 18:48:21 -0400

    Dom Access and manipulation

--- script.js
+++ script.js
@@ -702,3 +702,14 @@ var mike = {
 mike.calculaterAge = john.calculaterAge;
 mike.calculaterAge();
 */
+
+///Dom and Dom manipulation
+//DOCUMENT OBJECT MODEL
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = 6;
